6690fb29e55be391e2851a5c8f61c2048abb954f
Fabe278 <fabian.irger3@gmail.com>
2020-05-22 22:06:13 +0200

    Logout done

--- app/src/main/java/at/htlgkr/festlever/activities/MainActivity.java
+++ app/src/main/java/at/htlgkr/festlever/activities/MainActivity.java
@@ -7,6 +7,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +16,11 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 import at.htlgkr.festlever.R;
 import at.htlgkr.festlever.objects.User;
@@ -29,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
     private MainFragment privateFragment;
     private MainFragment myEventsFragment;
 
+    File rememberMeFile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +45,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        rememberMeFile = new File(getApplicationContext().getFilesDir().getPath().toString() + "/rememberMe.txt");
+        if(!rememberMeFile.exists()){
+            try {
+                rememberMeFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
@@ -69,5 +85,43 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main_options_menue, menu);
         return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.settings:
+
+                break;
+            case R.id.findFriends:
+
+                break;
+            case R.id.friendRequests:
+
+                break;
+            case R.id.eventRequests:
+
+                break;
+            case R.id.logout:
+                logout();
+                break;
+            default:
+
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    void logout(){
+        try (PrintWriter writer = new PrintWriter(new FileWriter(rememberMeFile, false))){
+            writer.write("");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
     }
 }
