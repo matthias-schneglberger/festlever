38e2442bab89418035bc14a1ed5742ac07f16c7e
Matthias Schneglberger <matthias.schneglberger@gmx.at>
2020-05-23 20:17:20 +0200

    done basic add event design

--- app/src/main/java/at/htlgkr/festlever/activities/CreateEventActivity.java
+++ app/src/main/java/at/htlgkr/festlever/activities/CreateEventActivity.java
@@ -1,10 +1,13 @@
 package at.htlgkr.festlever.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import at.htlgkr.festlever.R;
 import at.htlgkr.festlever.objects.User;
@@ -12,6 +15,7 @@ import at.htlgkr.festlever.objects.User;
 public class CreateEventActivity extends AppCompatActivity {
     private final String TAG = "CreateEventActivity";
     private User user;
+    private boolean eventIsPublic = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,30 @@ public class CreateEventActivity extends AppCompatActivity {
             user = (User) bundle.get("user");
             Log.d(TAG, "onCreate: Current User logged in: " + user.getUsername());
         }catch (NullPointerException ignored){}
+
+
+        final ConstraintLayout publicLayout = findViewById(R.id.activity_create_event_publicLayout);
+        final ConstraintLayout privateLayout = findViewById(R.id.activity_create_event_privateLayout);
+
+        publicLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                publicLayout.setBackgroundColor(getResources().getColor(R.color.addEventInputBackground));
+                privateLayout.setBackground(getDrawable(R.drawable.background_add_event_input_white));
+
+                eventIsPublic = true;
+            }
+        });
+
+        privateLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                publicLayout.setBackground(getDrawable(R.drawable.background_add_event_input_white));
+                privateLayout.setBackgroundColor(getResources().getColor(R.color.addEventInputBackground));
+
+                eventIsPublic = false;
+            }
+        });
 
     }
 }
