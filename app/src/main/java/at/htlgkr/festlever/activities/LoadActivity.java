17d5555a5adfd8b6163a70513bd94e685d9aa86e
Matthias Schneglberger <matthias.schneglberger@gmx.at>
2020-06-15 00:15:22 +0200

    added internet check in LoadActivity (Toast appears)

--- app/src/main/java/at/htlgkr/festlever/activities/LoadActivity.java
+++ app/src/main/java/at/htlgkr/festlever/activities/LoadActivity.java
@@ -2,9 +2,17 @@ package at.htlgkr.festlever.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import at.htlgkr.festlever.R;
 import at.htlgkr.festlever.logic.FireBaseCommunication;
@@ -13,12 +21,14 @@ import at.htlgkr.festlever.services.UpdateService;
 public class LoadActivity extends AppCompatActivity {
     FireBaseCommunication fireBaseCommunication = new FireBaseCommunication();
     ProgressBar progressBar;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load);
 
+        context = this;
         progressBar = findViewById(R.id.activity_load_progressBar);
         updateProgressBar(0);
         Thread thread = new Thread(this::doInBackground);
@@ -29,18 +39,27 @@ public class LoadActivity extends AppCompatActivity {
 
         // do smth here
 
-        updateProgressBar(25);
+        updateProgressBar(0);
 
-        // TODO network available
+        if(!isInternetAvailable()){
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(context, "Keine Internetverbindung!", Toast.LENGTH_SHORT).show();
+                }
+            });
 
-        updateProgressBar(50);
+            return;
+        }
+
+        updateProgressBar(33);
 
         startService(new Intent(this, UpdateService.class));
 
         fireBaseCommunication.getAllUsers();
 
 
-        updateProgressBar(75);
+        updateProgressBar(66);
 
         fireBaseCommunication.getAllEvents();
 
@@ -59,5 +78,20 @@ public class LoadActivity extends AppCompatActivity {
                 progressBar.setProgress(value);
             }
         });
+    }
+
+    public boolean isInternetAvailable() {
+        ConnectivityManager cm =
+                (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        try{
+            NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+            return activeNetwork != null &&
+                    activeNetwork.isConnectedOrConnecting();
+        }
+        catch (Exception e){
+            return false;
+        }
+
     }
 }
